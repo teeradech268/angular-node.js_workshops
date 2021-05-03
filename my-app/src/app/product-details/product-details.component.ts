0f8cfc86b6c72e76d8e3d314d4c6c399c92aaa8b
teeradech268 <ku.teeradech@gmail.com>
2021-05-03 15:41:35 +0700

    init

---
+++ my-app/src/app/product-details/product-details.component.ts
@@ -0,0 +1,19 @@
+/*import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { products } from '../products';
+
+export class ProductDetailsComponent implements OnInit{
+    product;
+
+    constructor(
+        private route: ActivatedRoute,
+    
+    ){}
+
+    /*ngOnInit() {
+        this.route.paramMap.subscribe(params =>{
+            this.product = products[+params.get('productId')];
+        })
+    }
+}*/
---
+++ my-app/src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screen'
+    },
+    {
+        name: 'Phone mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras'
+    },
+    {
+        name: 'Phone standard',
+        price: 299,
+        description: 'A best phone standard in the world'
+    }
+]
